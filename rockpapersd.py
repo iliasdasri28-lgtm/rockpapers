61ad6b6bfdbda210712e5fb43c4b13524420ab9a
iliasdasri28-lgtm <iliasdasri28@gmail.com>
2026-06-26 23:52:45 +0100

    Update rockpapersd.py

--- rockpapersd.py
+++ rockpapersd.py
@@ -2,37 +2,37 @@ import streamlit as st
 import random
 def rpsgame():
  try:
-  if st.button("Rock 🪨 "):
-    com_choice = random.choice(["rock","paper","scissor"])
+    if st.button("Rock 🪨 "):
+      com_choice = random.choice(["rock","paper","scissor"])
     
     if com_choice == "scissor" :
-      st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
-    elif com_choice == "paper" :
-      st.text("you lost ☹️")
-    elif com_choice == "rock" :
-      st.text("draw 🤝")
+        st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
+      elif com_choice == "paper" :
+        st.text("you lost ☹️")
+      elif com_choice == "rock" :
+        st.text("draw 🤝")
       
-  if st.button("Paper 📄 "):
-    com_choice = random.choice(["rock","paper","scissor"])
+    if st.button("Paper 📄 "):
+      com_choice = random.choice(["rock","paper","scissor"])
     
-    if com_choice == "rock" :
-      st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
-    elif com_choice == "scissor" :
-      st.text("you lost ☹️")
-    elif com_choice == "paper" :
-      st.text("draw 🤝")
+      if com_choice == "rock" :
+        st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
+      elif com_choice == "scissor" :
+        st.text("you lost ☹️")
+      elif com_choice == "paper" :
+        st.text("draw 🤝")
       
-  if st.button("Scissor ✂️ "):
-    com_choice = random.choice(["rock","paper","scissor"])
+    if st.button("Scissor ✂️ "):
+      com_choice = random.choice(["rock","paper","scissor"])
     
-    if com_choice == "paper" :
-      st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
-    elif com_choice == "rock" :
-      st.text("you lost ☹️")
-    elif com_choice == "scissor" :
-      st.text("draw 🤝")
+      if com_choice == "paper" :
+        st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
+      elif com_choice == "rock" :
+        st.text("you lost ☹️")
+      elif com_choice == "scissor" :
+        st.text("draw 🤝")
       
-  except:
+ except:
     st.text("error")
    
 if "page" not in st.session_state:
