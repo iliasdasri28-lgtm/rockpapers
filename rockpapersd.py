ef373409e0e5122542cecdf25bdae42532e9ed5b
iliasdasri28-lgtm <iliasdasri28@gmail.com>
2026-06-26 23:01:28 +0100

    Update rockpapersd.py

--- rockpapersd.py
+++ rockpapersd.py
@@ -3,21 +3,33 @@ import random
 try:
   if st.button("Rock 🪨 "):
     com_choice = random.choice(["rock","paper","scissor"])
-    if com_choice == "rock" :
-      st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
-    else:
-      st.text(f"you lost the computer chose {com_choice}")
-  if st.button("Paper 📄 "):
-    com_choice = random.choice(["rock","paper","scissor"])
-    if com_choice == "paper" :
-      st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
-    else:
-      st.text(f"you lost the computer chose {com_choice}")
-  if st.button("Scissor ✂️ "):
-    com_choice = random.choice(["rock","paper","scissor"])
+    
     if com_choice == "scissor" :
       st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
-    else:
-      st.text(f"you lost the computer chose {com_choice}")
+    elif com_choice == "paper" :
+      st.text("you lost ☹️")
+    elif com_choice == "rock" :
+      st.text("draw 🤝")
+      
+  if st.button("Paper 📄 "):
+    com_choice = random.choice(["rock","paper","scissor"])
+    
+    if com_choice == "rock" :
+      st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
+    elif com_choice == "scissor" :
+      st.text("you lost ☹️")
+    elif com_choice == "paper" :
+      st.text("draw 🤝")
+      
+  if st.button("Scissor ✂️ "):
+    com_choice = random.choice(["rock","paper","scissor"])
+    
+    if com_choice == "paper" :
+      st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
+    elif com_choice == "rock" :
+      st.text("you lost ☹️")
+    elif com_choice == "scissor" :
+      st.text("draw 🤝")
+      
 except:
   st.text("error")
