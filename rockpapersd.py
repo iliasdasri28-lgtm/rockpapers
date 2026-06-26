51a57ead6057df8880a3f2b98dc128807ac74fcb
iliasdasri28-lgtm <iliasdasri28@gmail.com>
2026-06-26 22:39:27 +0100

    Update rockpapersd.py

--- rockpapersd.py
+++ rockpapersd.py
@@ -6,18 +6,18 @@ try:
     if com_choice == "rock" :
       st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
     else:
-      st.text(f"you lost the computer chose {com_choice})
+      st.text(f"you lost the computer chose {com_choice}")
   if st.button("Paper 📄 "):
     com_choice = random.choice(["rock","paper","scissor"])
     if com_choice == "paper" :
       st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
     else:
-      st.text(f"you lost the computer chose {com_choice})
+      st.text(f"you lost the computer chose {com_choice}")
   if st.button("Scissor ✂️ "):
     com_choice = random.choice(["rock","paper","scissor"])
     if com_choice == "scissor" :
       st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
     else:
-      st.text(f"you lost the computer chose {com_choice})
+      st.text(f"you lost the computer chose {com_choice}")
 except:
   st.text("error")
