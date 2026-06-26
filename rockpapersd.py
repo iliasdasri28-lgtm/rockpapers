be373b549f7bf35615ea259dde3477f621b1f786
iliasdasri28-lgtm <iliasdasri28@gmail.com>
2026-06-26 22:37:39 +0100

    Update rockpapersd.py

--- rockpapersd.py
+++ rockpapersd.py
@@ -1,19 +1,19 @@
 import streamlit  as st
 import random
 try:
-  if st.button("Rock 🪨 ")
+  if st.button("Rock 🪨 "):
     com_choice = random.choice(["rock","paper","scissor"])
     if com_choice == "rock" :
       st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
     else:
       st.text(f"you lost the computer chose {com_choice})
-  if st.button("Paper 📄 ")
+  if st.button("Paper 📄 "):
     com_choice = random.choice(["rock","paper","scissor"])
     if com_choice == "paper" :
       st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
     else:
       st.text(f"you lost the computer chose {com_choice})
-  if st.button("Scissor ✂️ ")
+  if st.button("Scissor ✂️ "):
     com_choice = random.choice(["rock","paper","scissor"])
     if com_choice == "scissor" :
       st.text(f"congratulations 🎉 you won computer choice is {com_choice}")
