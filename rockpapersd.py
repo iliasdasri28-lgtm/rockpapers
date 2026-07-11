dae007c2b8c874f0c358a573498136478d3904ed
ILYAS <iliasdasri28@gmail.com>
2026-07-11 18:28:47 +0100

    hi

--- rockpapersd.py
+++ rockpapersd.py
@@ -68,33 +68,33 @@ def agecal():
  if st.button("Calculate"):
     try:
         
-        daydate,monthdate,yeardate = date.split("/")
+      daydate,monthdate,yeardate = date.split("/")
         
-        today = int(daydate)
-        tomonth = int(monthdate)
-        toyear = int(yeardate)
+      today = int(daydate)
+      tomonth = int(monthdate)
+      toyear = int(yeardate)
         
-        born_day,born_month,born_year=born.split("/")
+      born_day,born_month,born_year=born.split("/")
         
-        day = int(born_day)
-        month = int(born_month)
-        year = int(born_year)
+      day = int(born_day)
+      month = int(born_month)
+      year = int(born_year)
         
-        if month > tomonth or (month == tomonth and day > today) : 
+      if month > tomonth or (month == tomonth and day > today) : 
             ageyear = toyear - year - 1
             
-        else:
+      else:
             ageyear = toyear - year
             
-        if day > today :
+      if day > today :
             agemonth = (tomonth - month - 1)%12
             ageday = (today + 30) - day
             
-        else:
+      else:
             agemonth = (tomonth - month)%12
             ageday = today - day
             
-        st.write(f"your age is {ageyear} year and {agemonth} month and {ageday} day")
+      st.write(f"your age is {ageyear} year and {agemonth} month and {ageday} day")
     except:
           st.error("enter the date as number in that form: xx/yy/zzzz")
  
